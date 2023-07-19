9359a7ac131a7fd50a491933f995891df317de9e
Daniel Wheaton <dwheaton2011@gmail.com>
2023-07-19 15:09:02 -0500

    Add files via upload

--- Assignments6and7/brintondata.js
+++ Assignments6and7/brintondata.js
@@ -1288,4 +1288,4 @@ var brintondata = {
 { "type": "Feature", "properties": { "name": "Opera House", "latitude": 41.178903, "longitude": -92.251849, "time": "12/17/1908" }, "geometry": { "type": "Point", "coordinates": [ -92.251849, 41.178903 ] } },
 { "type": "Feature", "properties": { "name": "Opera House", "latitude": 41.198372, "longitude": -92.093089, "time": "12/18/1908" }, "geometry": { "type": "Point", "coordinates": [ -92.093089, 41.198372 ] } }
 ]
-}
+};
